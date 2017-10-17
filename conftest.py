6a689ac1ba6f1d078e235fc45f1e1b14dac2a0ae
Og B. Maciel <omaciel@ogmaciel.com>
2017-10-17 17:50:28 -0400

    Added validation around parameters during collection.

--- conftest.py
+++ conftest.py
@@ -7,9 +7,17 @@ def pytest_generate_tests(metafunc):
     if hasattr(metafunc.function, 'gen_string'):
         # We should have at least the first 2 arguments to gen_string
         args = metafunc.function.gen_string.args
-        if len(args) < 2:
-            args = (None, None)
+        if len(args) == 0:
+            args = (1, None)
+        elif len(args) == 1:
+            args = (args[0], None)
         items, str_type = args
+        if not isinstance(items, int):
+            raise pytest.UsageError(
+                'Mark expected an integer, got a {}: {}'.format(
+                    type(items), items))
+        if items < 1:
+            raise pytest.UsageError('Mark expected an integer greater than 0, got {}'.format(items))
         kwargs = metafunc.function.gen_string.kwargs
         data = gen_string(items, str_type, *args[2:], **kwargs)
         metafunc.parametrize('value', data)
--- test_pytest_fauxfactory.py
+++ test_pytest_fauxfactory.py
@@ -2,9 +2,20 @@
 import pytest
 
 
+def is_numeric(value):
+    '''Check if value is numeric.'''
+    return value.isnumeric()
+
+
 @pytest.mark.gen_string()
 def test_gen_alpha_string_with_no_arguments(value):
     '''Passing no arguments should return a random string type.'''
+    assert len(value) > 0
+
+
+@pytest.mark.gen_string(1)
+def test_gen_alpha_string_with_limit_arguments(value):
+    '''Passing limit argument should return a random string type.'''
     assert len(value) > 0
 
 
@@ -12,3 +23,9 @@ def test_gen_alpha_string_with_no_arguments(value):
 def test_gen_alpha_string_with_length(value):
     '''Generate an `alpha` string of length 12.'''
     assert len(value) == 12
+
+
+@pytest.mark.gen_string(1, 'punctuation', length=12, validator=is_numeric, default='1')
+def test_gen_alpha_string_with_validator(value):
+    '''Call `gen_string` with validator that returns default of `1`.'''
+    assert value == '1'
