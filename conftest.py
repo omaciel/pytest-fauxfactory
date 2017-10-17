8fd43f740357ae9eba17e9cfb031c161a233c466
Og B. Maciel <omaciel@ogmaciel.com>
2017-10-17 16:59:05 -0400

    Plugin now works as a `pytest.mark` to generate random strings.
    The plugin now works as a `pytest.mark` to generate random strings for your
    tests.

--- conftest.py
+++ conftest.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
+import pytest
+from pytest_fauxfactory import gen_string
+
+
+def pytest_generate_tests(metafunc):
+    if hasattr(metafunc.function, 'gen_string'):
+        # We should have at least the first 2 arguments to gen_string
+        args = metafunc.function.gen_string.args
+        if len(args) < 2:
+            args = (None, None)
+        items, str_type = args
+        kwargs = metafunc.function.gen_string.kwargs
+        data = gen_string(items, str_type, *args[2:], **kwargs)
+        metafunc.parametrize('value', data)
+
+
 pytest_plugins = ("pytest_fauxfactory", )  # Specify the plugin so py.test finds it on Travis
--- pytest_fauxfactory.py
+++ pytest_fauxfactory.py
@@ -1,5 +1,27 @@
 # -*- coding: utf-8 -*-
-def pytest_namespace():
-    """Inject the fauxfactory module into the pytest namespace using ``faux`` name."""
-    import fauxfactory
-    return {'faux': fauxfactory}
+import fauxfactory
+
+STRING_TYPES = (
+    'alpha',
+    'alphanumeric',
+    'cjk',
+    'html',
+    'latin1',
+    'numeric',
+    'utf8',
+    'punctuation',
+)
+
+
+def gen_string(items=None, str_type=None, *args, **kwargs):
+    '''Generate a new string type.'''
+    item = 0
+
+    if items is None:
+        items = 1
+    if str_type is None:
+        str_type = fauxfactory.gen_choice(STRING_TYPES)
+
+    while item < items:
+        yield fauxfactory.gen_string(str_type, *args, **kwargs)
+        item += 1
