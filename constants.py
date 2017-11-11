038a8457774461b100a9cd4daf6bda472e333f47
Og B. Maciel <omaciel@ogmaciel.com>
2017-11-11 12:27:42 -0500

    Major refactoring applied to entire package.
    I have split the code originally found in `pytest_fauxfactory` into several
    different modules, their intentions which I hope will be clear by their names:
    
    * constants: keep any constants used throughout code handlers: functions which
    * will be used to handle diffetent marks marks: these are the mark functions
    * themselves
    
    The final version of `pytest_fauxfactory` is much smaller, cleaner, and
    hopefully easier to maintain and understand.

---
+++ constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Constants used by pytest-fauxfactory."""
+STRING_TYPES = (
+    'alpha',
+    'alphanumeric',
+    'cjk',
+    'html',
+    'latin1',
+    'numeric',
+    'utf8',
+    'punctuation',
+)
---
+++ marks.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""FauxFactory specific marks methods."""
+from itertools import chain
+
+import fauxfactory
+
+from constants import STRING_TYPES
+
+
+def faux_callable(items, callable_func, *args, **kwargs):
+    """Generate new values from callable object."""
+    if items is None:
+        items = 1
+    for _ in range(items):
+        yield callable_func(*args, **kwargs)
+
+
+def faux_generator(*args):
+    """Generate values from generators passed as arguments."""
+    return chain.from_iterable(args)
+
+
+def faux_string(items, str_type=None, *args, **kwargs):
+    """Generate a new string type."""
+    item = 0
+
+    if str_type is None:
+        str_type = fauxfactory.gen_choice(STRING_TYPES)
+
+    while item < items:
+        yield fauxfactory.gen_string(str_type, *args, **kwargs)
+        item += 1
