ef6eec231f70655274650c376510d8acab272702
Og B. Maciel <omaciel@ogmaciel.com>
2017-12-06 14:22:07 -0500

    Updated setup.py and added new HISTORY.rst file.

---
+++ HISTORY.rst
@@ -0,0 +1,22 @@
+.. :changelog:
+
+Release History
+===============
+
+1.1.0 (2017-12-06)
+------------------
+
+- Renamed `gen_string` mark to `faux_string` with extended capabilities
+- Added new `faux_callable` and `faux_generator` marks
+- Updated README file to include usable examples
+
+1.0.1 (2017-10-18)
+------------------
+
+- First release as a Pytest plugin exposing a `gen_string` mark
+- Tests can be decorated with new mark to generate random strings as parameters
+
+1.0.0 (2017-12-06)
+------------------
+
+- First release.
--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 # -*- encoding: utf-8 -*-
 """Setup file for the plugin."""
+import codecs
+import os
+
 from setuptools import setup
+
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    filename = os.path.join(*paths)
+    with codecs.open(filename, mode='r', encoding='utf-8') as handle:
+        return handle.read()
+
+
+LONG_DESCRIPTION = (read('README.rst') + '\n\n' +
+                    read('AUTHORS.rst') + '\n\n' +
+                    read('HISTORY.rst'))
+
 
 setup(
     name='pytest-fauxfactory',
-    version='1.0.1',
+    version='1.1.0',
     author='Og Maciel',
     author_email='omaciel@ogmaciel.com',
     description='Integration of fauxfactory into pytest.',
+    long_description=LONG_DESCRIPTION,
     license='GPLv3',
     keywords='pytest',
     url='https://github.com/omaciel/pytest-fauxfactory',
@@ -25,7 +42,16 @@ setup(
     entry_points={'pytest11': ['fauxfactory = pytest_fauxfactory.plugin']},
     classifiers=[
         'Topic :: Utilities',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Unix Shell'
-    ]
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Testing',
+    ],
+    test_suite='tests',
 )
