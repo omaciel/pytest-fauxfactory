87221668f60449c91ac7e678e9c8efa096198a78
Og B. Maciel <omaciel@ogmaciel.com>
2017-12-06 14:53:30 -0500

    Make sure to include rst files in package.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ LONG_DESCRIPTION = (read('README.rst') + '\n\n' +
 
 setup(
     name='pytest-fauxfactory',
-    version='1.1.0',
+    version='1.1.1',
     author='Og Maciel',
     author_email='omaciel@ogmaciel.com',
     description='Integration of fauxfactory into pytest.',
